f1d001a89faa96c561ac68fcb96c7ec2aa832095
Jantu Deb <jantud54@gmail.com>
2022-03-28 14:17:08 +0530

    added reset css and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import History from "./pages/history/History";
+import Home from "./pages/home/Home";
+import Layout from "./pages/Layout";
+import LikedVideo from "./pages/liked-videos/LikedVideo";
+import PlayList from "./pages/playlist/PlayList";
+import VideoDetails from "./pages/video-details/VideoDetails";
+import WatchLater from "./pages/watchlater/WatchLater";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={ <Layout><Home /></Layout>} />
+        <Route path="/playlist" element={ <Layout><PlayList /></Layout>} />
+        <Route path="/watch-later" element={ <Layout><WatchLater /></Layout>} />
+        <Route path="/liked-videos" element={ <Layout><LikedVideo /></Layout>} />
+        <Route path="/history" element={ <Layout><History /></Layout>} />
+        <Route path="/video" element={<VideoDetails />} />
+      </Routes>
   );
 }
 
