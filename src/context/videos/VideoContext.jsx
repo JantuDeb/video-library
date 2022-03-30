21204d5b5765a05c6f0fe4c06d7e7e1dbff0cbdd
Jantu Deb <jantud54@gmail.com>
2022-03-30 20:06:43 +0530

    fix: added missing files for build to work

---
+++ src/context/videos/VideoContext.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { axiosInstance } from "../../utils/axios-instance";
+
+const VideoContext = createContext([]);
+const VideoProvider = ({ children }) => {
+  const [videos, setVideos] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  useEffect(() => {
+    (async () => {
+      const [videoResponse, categoryResponse] = await Promise.all([
+        axiosInstance.get("/videos"),
+        axiosInstance.get("/categories"),
+      ]);
+      setVideos(videoResponse.data.videos);
+      setCategories(categoryResponse.data.categories);
+    })();
+  }, []);
+
+  const setSelectedCategoryFilter = (id) => setSelectedCategory(id);
+  const fileterVideosByCategory = selectedCategory
+    ? videos.filter((video) => video.categoryId === selectedCategory)
+    : videos;
+
+  console.log(fileterVideosByCategory);
+  return (
+    <VideoContext.Provider
+      value={{
+        videos: fileterVideosByCategory,
+        categories,
+        setSelectedCategoryFilter,
+      }}
+    >
+      {children}
+    </VideoContext.Provider>
+  );
+};
+const useVideos = () => useContext(VideoContext);
+export { VideoProvider, useVideos };
