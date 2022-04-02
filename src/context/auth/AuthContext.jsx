4040abf7c2a82b630f7c92cf236f283e7be5ef1c
Jantu Deb <jantud54@gmail.com>
2022-04-02 13:57:39 +0530

    fixed dependency issue

--- src/context/auth/AuthContext.jsx
+++ src/context/auth/AuthContext.jsx
@@ -42,6 +42,7 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     user && authDispatch({ type: LOGIN, payload: user });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const logOut = async () => {
